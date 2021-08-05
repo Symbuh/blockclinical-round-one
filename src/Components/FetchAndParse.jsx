09c8cf66ccc1e0f91c3a968675b318b854d6f747
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-08-05 16:25:14 -0700

    Initial boiler plate complete, working on fetching data and setting up form

---
+++ src/Components/FetchAndParse.jsx
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import react, { useState, useEffect } from 'react';
+
+const FetchAndParse = () => {
+  const [questions, setQuestions] = useState('')
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
+  const fetchImages = () => {
+    axios.get('https://app.blockclinical.com/api/recruit/questions')
+    .then(response => setQuestions(response.data.info.questions))
+    .catch(err => console.log(err))
+  }
+
+  return (
+    <div>
+      Please Fill out fields and stuff
+    </div>
+  )
+}
+
