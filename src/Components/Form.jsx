82af9d87c00a62fd6bc6ae88934879a15879a64a
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-08-06 14:01:29 -0700

    Submit button in progress

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,16 +1,20 @@
 import react, { useState, useEffect } from 'react'
+import SubmitForm from './SubmitForm'
 
 const Form = ({ questions }) => {
   console.log(questions)
 
   const [answers, setAnswers] = useState([])
+  const [formComplete, setFormComplete] = useState(false)
 
   const handleChange = (e) => {
     setAnswers({
       ...answers,
       [e.target.name]: e.target.value
+    })
+    if (Object.keys(answers).length === questions.length) {
+      setFormComplete(true)
     }
-    )
   }
   return (
   <div>
@@ -30,6 +34,7 @@ const Form = ({ questions }) => {
         </div>
       )})
     }
+    <SubmitForm answers={answers} formComplete={formComplete} />
   </div>
   )
 }
