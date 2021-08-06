4bf4ed67f4c3a66e5b222687d9d2d4f1ac179cf8
Nicholas Sabadicci <nicholasrsabadicci@gmail.com>
2021-08-06 13:53:23 -0700

    Saving form response values within state.

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,16 +3,30 @@ import react, { useState, useEffect } from 'react'
 const Form = ({ questions }) => {
   console.log(questions)
 
+  const [answers, setAnswers] = useState([])
+
+  const handleChange = (e) => {
+    setAnswers({
+      ...answers,
+      [e.target.name]: e.target.value
+    }
+    )
+  }
   return (
   <div>
     {
       questions.map((question) => {
       return (
-        <div>
+        <div key={question.id}>
           <div>
             {question.question}
           </div>
-          <input type="text"></input>
+          <input
+            type="text"
+            className="textInput"
+            name={question.id}
+            onChange={handleChange}
+          ></input>
         </div>
       )})
     }
